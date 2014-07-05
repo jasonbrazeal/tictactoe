4460f3181ff32b9ae8befd7535ea76597917160b
Jason Brazeal <jsonbrazeal@gmail.com>
2014-07-05 15:53:28 -0400

    added Game model

--- apps/game/models.py
+++ apps/game/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+    '''Tic-tac-toe game
+    '''
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    board = models.CharField(max_length=54) # max length: [None, None, None, None, None, None, None, None, None]
+    player1 = models.CharField(max_length=1) # X, O
+    player2 = models.CharField(max_length=1) # X, O
+    winner = models.CharField(max_length=4) # X, O, None
