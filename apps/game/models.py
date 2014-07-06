432994f869e4ac24d99120ae99ee279d58514708
Jason Brazeal <jsonbrazeal@gmail.com>
2014-07-06 11:21:15 -0400

    added Game model

--- apps/game/models.py
+++ apps/game/models.py
@@ -6,7 +6,47 @@ class Game(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
 
-    board = models.CharField(max_length=54) # max length: [None, None, None, None, None, None, None, None, None]
-    player1 = models.CharField(max_length=1) # X, O
-    player2 = models.CharField(max_length=1) # X, O
-    winner = models.CharField(max_length=4) # X, O, None
+    board_str = models.CharField(max_length=54, default=[None]*9) # max length: [None, None, None, None, None, None, None, None, None]
+    player1 = models.CharField(max_length=1, default='X') # X, O
+    player2 = models.CharField(max_length=1, default='O') # X, O
+    winner = models.CharField(max_length=4, default=None) # X, O, None
+
+    def _get_board(self):
+        '''Returns the tic-tac-toe board as a Python list object.
+        '''
+        return eval(self.board_str)
+
+    board = property(_get_board)
+
+    def get_winner(self):
+        if ((self.board[3] == self.board[4] == self.board[5])
+        or (self.board[1] == self.board[4] == self.board[7])
+        or (self.board[0] == self.board[4] == self.board[8])
+        or (self.board[2] == self.board[4] == self.board[6])):
+            return self.board[4] # center
+
+        elif ((self.board[0] == self.board[1] == self.board[2])
+        or (self.board[0] == self.board[3] == self.board[6])):
+            return self.board[0] # top left
+
+        elif ((self.board[6] == self.board[7] == self.board[8])
+        or (self.board[2] == self.board[5] == self.board[8])):
+            return self.board[8] # bottom right
+
+        # returns None
+
+    def is_tie(self):
+        '''Returns None
+        '''
+        if not self.get_winner():
+            return None not in self.board
+        # returns None
+
+    # def _get_full_name(self):
+    #     "Returns the person's full name."
+    #     return u'%s %s' % (self.first_name, self.last_name)
+    # full_name = property(_get_full_name)
+
+
+    def __unicode__(self):
+        return self.board_str
