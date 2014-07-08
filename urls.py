cc929fd336bbcefc6f492c7da4c5f6a4686195fc
Jason Brazeal <jsonbrazeal@gmail.com>
2014-07-08 10:23:40 -0400

    set up django admin

--- urls.py
+++ urls.py
@@ -2,14 +2,14 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 
-# from django.contrib import admin
+from django.contrib import admin
 
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
 
     url(r'^', include('game.urls')),
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
 
 if settings.DEBUG and settings.STATIC_ROOT:
