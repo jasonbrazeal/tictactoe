3fe4a5e83403f9e83c19433ec53bb7aae0bba9a2
Jason Brazeal <jsonbrazeal@gmail.com>
2014-10-12 11:08:39 -0400

    changed db location

--- settings/dev.py
+++ settings/dev.py
@@ -12,8 +12,8 @@ SERVER_EMAIL = ''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        # /var/www/db and actual db file must be owned by apache, vagrant group, permissions 764
-        'NAME': '/var/www/db/tictactoe_dev.db',                      # Or path to database file if using sqlite3.
+        # path and actual db file must be owned by apache, vagrant group, permissions 764
+        'NAME': '/opt/tictactoe/tictactoe_dev.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
